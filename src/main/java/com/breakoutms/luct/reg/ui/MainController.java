78be3dee61c77be52152281784406f2568c6ac6f
Ntholi Nkhatho <ntholi@live.com>
2020-08-13 10:28:12 +0200

    Add main controller

---
+++ src/main/java/com/breakoutms/luct/reg/ui/MainController.java
@@ -0,0 +1,18 @@
+package com.breakoutms.luct.reg.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ScrollPane;
+
+public class MainController {
+
+    @FXML private ProgressBar progressBar;
+    @FXML private Button ok;
+    @FXML private ScrollPane scrollPane;
+
+    @FXML
+    void initialize() {
+    	
+    }
+}
